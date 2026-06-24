55ec737cd0373e598d0fc9ee6d6ac37b5c78275d
João Carlos <168746342+Joaocarlos007@users.noreply.github.com>
2026-06-24 16:03:28 -0300

    fase 1 miros.h

--- Core/Inc/miros.h
+++ Core/Inc/miros.h
@@ -8,13 +8,22 @@
 #ifndef INC_MIROS_H_
 #define INC_MIROS_H_
 
+#include <cstdint>  // tipos de inteiros com tamanhos de bits exatos
+
 namespace rtos {
 /* Thread Control Block (TCB) */
 typedef struct {
     void *sp; /* stack pointer */
     uint32_t timeout; /* timeout delay down-counter */
+    uint8_t id;  // guardar o índice da tarefa
     /* ... other attributes associated with a thread */
 } OSThread;
+
+/* Semáforo de contagem, sem busy-waiting */
+typedef struct{
+	uint32_t count;   // contador
+	uint32_t waitSet; // bitmask das tarefas bloqueadas nesse semáforo
+} OS_Semaphore;
 
 const uint16_t TICKS_PER_SEC = 100U;
 
@@ -44,6 +53,20 @@ void OSThread_start(
     OSThread *me,
     OSThreadHandler threadHandler,
     void *stkSto, uint32_t stkSize);
+
+/* ---------- Fase 1 APIs ---------- */
+
+// cede voluntariamente a CPU para a próxima tarefa pronta
+void OS_yield(void);
+
+// inicializa um semáforo com um número inicial no contador
+void OS_Semaphore_init(OS_Semaphore *sem, uint32_t initCount);
+
+// tenta adquirir o semáforo e bloqueia a tarefa se count = 0
+void OS_semWait(OS_Semaphore *sem);
+
+// libera o semáforo e se houver tarefa bloqueada, desbloqueia uma
+void OS_semSignal(OS_Semaphore *sem);
 
 }
 
